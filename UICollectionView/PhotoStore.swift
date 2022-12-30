39e19d59de5020fd71c51504a628c53738c2f551
Tianbo <tianbo@hey.com>
2022-12-30 14:48:57 -0800

    in memory and file cache for photos

--- UICollectionView/PhotoStore.swift
+++ UICollectionView/PhotoStore.swift
@@ -14,6 +14,9 @@ enum PhotoError: Error {
 
 // handle requests, works as a remote store
 class PhotoStore {
+    
+    let imageStore = ImageStore()
+    
     private let session: URLSession = {
         let config = URLSessionConfiguration.default
         return URLSession(configuration: config)
@@ -40,6 +43,15 @@ class PhotoStore {
     }
     
     func fetchImage(for photo: Photo, completion: @escaping (Result<UIImage, Error>) -> Void) {
+        let photoKey = photo.photoID
+        if let image = imageStore.image(forKey: photoKey) {
+            OperationQueue.main.addOperation {
+                completion(.success(image))
+            }
+            return
+        }
+        
+        
         guard let photoURL = photo.remoteURL, let url = URL(string: photoURL) else {
             completion(.failure(PhotoError.missingImageURL))
             return
@@ -48,6 +60,11 @@ class PhotoStore {
         
         let task = session.dataTask(with: request) { (data, response, error) in
             let result = self.processImageResponse(data: data, error: error)
+            
+            if case let .success(image) = result {
+                self.imageStore.setImage(image, forKey: photoKey)
+            }
+            
             OperationQueue.main.addOperation {
                 completion(result)
             }
