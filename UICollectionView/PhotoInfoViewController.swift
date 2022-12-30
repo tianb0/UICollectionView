ae995288a7e1b07f5b4f11c7dcc54ea4a0398e94
Tianbo <tianbo@hey.com>
2022-12-30 15:00:50 -0800

    tap on cell

---
+++ UICollectionView/PhotoInfoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoInfoViewController.swift
+//  UICollectionView
+//
+//  Created by Tianbo Qiu on 12/30/22.
+//
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var photo: Photo! {
+        didSet {
+            navigationItem.title = photo.title
+        }
+    }
+    
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchImage(for: photo) { (result) in
+            switch result {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error fetching image for photo: \(error)")
+            }
+        }
+    }
+}
